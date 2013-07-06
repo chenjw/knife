d74ca62350dd10468ddef4fdf428eca8d4d8378d
chenjw <cjw1983@gmail.com>
2013-07-06 17:53:45 +0800

    update

--- shell/knife.sh
+++ shell/knife.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-java -cp .:lib/sinetfactory.jar:lib/jline-1.0.jar:lib/fastjson-1.1.17.jar:lib/knife-client.jar:$JAVA_HOME/lib/tools.jar com.chenjw.knife.client.ClientMain $*
+cp $JAVA_HOME/lib/tools.jar lib/tools.jar
+java -cp .:lib/sinetfactory.jar:lib/jline-1.0.jar:lib/fastjson-1.1.17.jar:lib/knife-client.jar:lib/tools.jar com.chenjw.knife.client.ClientMain $*
