4c242ba8f57baa1228f11bfc9e218fc47b5a8bc2
chenjw <chenjw@chenjw-ubuntu>
2014-02-28 22:05:18 +0800

    11

--- client/java/com/chenjw/knife/client/utils/InetHelper.java
+++ client/java/com/chenjw/knife/client/utils/InetHelper.java
@@ -16,110 +16,115 @@ import com.jscape.inet.ssh.util.SshParameters;
 
 public class InetHelper {
 
-	public static void scp(String hostname, String username, String password,
-			String srcPath, String targetPath) {
-		SshParameters params = new SshParameters(hostname, username, password);
+    public static void scp(String hostname, String username, String password,
+            String srcPath, String targetPath) {
+        SshParameters params = new SshParameters(hostname, username, password);
 
-		Scp scp = new Scp(params);
+        Scp scp = new Scp(params);
 
-		try {
+        try {
 
-			scp.connect();
-			File f = new File(srcPath);
-			if (f.isDirectory()) {
-				scp.uploadDir(f, targetPath);
-			} else {
-				scp.upload(f, targetPath);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		scp.disconnect();
-	}
+            scp.connect();
+            File f = new File(srcPath);
+            if (f.isDirectory()) {
+                scp.uploadDir(f, targetPath);
+            } else {
+                scp.upload(f, targetPath);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        scp.disconnect();
+    }
 
-	public static SshClient ssh(String hostname, String username,
-			String password) {
+    public static SshClient ssh(String hostname, String username,
+            String password) {
 
-		try {
-			SshParameters sshParams = new SshParameters(hostname, username,
-					password);
-			final StringBuffer sb = new StringBuffer();
-			final BlockingQueue<String> lines = new ArrayBlockingQueue<String>(
-					1);
-			final Ssh ssh = new Ssh(sshParams);
-			ssh.addSshListener(new SshListener() {
+        try {
+            SshParameters sshParams = new SshParameters(hostname, username,
+                    password);
+            final StringBuffer sb = new StringBuffer();
+            final BlockingQueue<String> lines = new ArrayBlockingQueue<String>(
+                    1);
+            final Ssh ssh = new Ssh(sshParams);
+            ssh.addSshListener(new SshListener() {
 
-				@Override
-				public void connected(SshConnectedEvent arg0) {
+                @Override
+                public void connected(SshConnectedEvent arg0) {
 
-				}
+                }
 
-				@Override
-				public void dataReceived(SshDataReceivedEvent arg0) {
-					String str = arg0.getData();
-					if (str != null) {
-						for (char c : str.toCharArray()) {
-							if (c == '\n') {
-								try {
-									lines.put(sb.toString());
-								} catch (InterruptedException e) {
-								}
-								sb.delete(0, sb.length());
-							} else {
-								sb.append(c);
-							}
+                @Override
+                public void dataReceived(SshDataReceivedEvent arg0) {
+                    String str = arg0.getData();
+                    if (str != null) {
+                        for (char c : str.toCharArray()) {
+                            if (c == '\n') {
+                                try {
+                                    lines.put(sb.toString());
+                                } catch (InterruptedException e) {
+                                }
+                                sb.delete(0, sb.length());
+                            } else {
+                                sb.append(c);
+                            }
 
-						}
-					}
+                        }
+                    }
 
-				}
+                }
 
-				@Override
-				public void disconnected(SshDisconnectedEvent arg0) {
+                @Override
+                public void disconnected(SshDisconnectedEvent arg0) {
 
-				}
-			});
-			ssh.setReadTimeout(0);
-			ssh.setTimeout(0);
-			ssh.connect();
+                }
+            });
+            ssh.setReadTimeout(0);
+            ssh.setTimeout(0);
+            ssh.connect();
 
-			final OutputStream out = ssh.getOutputStream();
+            final OutputStream out = ssh.getOutputStream();
 
-			return new SshClient() {
-				@Override
-				public void exec(String command) {
-					if (command != null) {
-						String line = command + "\n";
-						try {
-							out.write(line.getBytes());
-							out.flush();
-						} catch (Exception ioe) {
-							ioe.printStackTrace();
-						}
-					}
-				}
+            return new SshClient() {
+                @Override
+                public void exec(String command) {
+                    if (command != null) {
+                        String line = command + "\n";
+                        try {
+                            out.write(line.getBytes());
+                            out.flush();
+                        } catch (Exception ioe) {
+                            ioe.printStackTrace();
+                        }
+                    }
+                }
 
-				@Override
-				public void close() {
-					ssh.disconnect();
+                @Override
+                public void close() {
+                    ssh.disconnect();
 
-				}
+                }
 
-				@Override
-				public String readLine() {
-					try {
-						return lines.take();
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-						return null;
-					}
-				}
+                @Override
+                public String readLine() {
+                    try {
+                        return lines.take();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                        return null;
+                    }
+                }
 
-			};
+            };
 
-		} catch (Exception e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    
+    public static void main(String[] args) {
+        InetHelper.scp("aggrbillinfo.d934aqcn.alipay.net", "admin", "Aqc_paas",
+            "/home/chenjw/my_workspace/knife/lib/lib", "/tmp/");
+    }
 }
