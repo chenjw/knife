aaf2c34be4f87d8ef42bd487d184b7ef450d6239
chenjw <cjw1983@gmail.com>
2018-01-02 16:36:48 +0800

    update

--- client/java/com/chenjw/knife/client/console/CommandConsoleTemplate.java
+++ client/java/com/chenjw/knife/client/console/CommandConsoleTemplate.java
@@ -20,220 +20,232 @@ import com.chenjw.knife.core.model.VMDescriptor;
 import com.chenjw.knife.utils.StringHelper;
 
 public abstract class CommandConsoleTemplate extends CommandListenerTemplate {
-	private FileWriter fileLog;
-	{
-		try {
-			File file = new File("knife.log");
-			fileLog = new FileWriter(file, true);
-			System.out.println("log to " + file.getAbsolutePath());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	private FormaterManager formaterManager = new FormaterManager(
-			new ClientPrinter(), this, new ClientCompletorProcesser());
+  private FileWriter fileLog;
+  {
+    try {
+      File file = new File("knife.log");
+      fileLog = new FileWriter(file, true);
+      System.out.println("log to " + file.getAbsolutePath());
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+  private FormaterManager formaterManager = new FormaterManager(new ClientPrinter(), this,
+      new ClientCompletorProcesser());
 
-	public CommandConsoleTemplate() {
-		this.callBack = new CommandResponseCallback();
-	}
+  public CommandConsoleTemplate() {
+    this.callBack = new CommandResponseCallback();
+  }
 
-	@SuppressWarnings("unchecked")
-	private List<VMDescriptor> sendListVm() {
-		Command c = new Command();
-		c.setName(Constants.REQUEST_LIST_VM);
+  @SuppressWarnings("unchecked")
+  private List<VMDescriptor> sendListVm() {
+    Command c = new Command();
+    c.setName(Constants.REQUEST_LIST_VM);
 
-		Result r = sendSyncCommand(c);
-		// 获得虚拟机列表
-		List<VMDescriptor> vmList = (List<VMDescriptor>) r.getContent();
-		if (vmList.isEmpty()) {
-			handleText("cant find vm process!");
-			return null;
-		}
+    Result r = sendSyncCommand(c);
+    // 获得虚拟机列表
+    List<VMDescriptor> vmList = (List<VMDescriptor>) r.getContent();
+    if (vmList.isEmpty()) {
+      handleText("cant find vm process!");
+      return null;
+    }
 
-		for (int i = 0; i < vmList.size(); i++) {
-			handleText(i + ". " + vmList.get(i).getPid() + " "
-					+ vmList.get(i).getName());
-		}
-		String msg = "input [0-" + (vmList.size() - 1) + "] to choose vm! ";
-		handleText(msg);
-		return vmList;
-	}
+    for (int i = 0; i < vmList.size(); i++) {
+      handleText(i + ". " + vmList.get(i).getPid() + " " + vmList.get(i).getName());
+    }
+    String msg = "input [0-" + (vmList.size() - 1) + "] to choose vm! ";
+    handleText(msg);
+    return vmList;
+  }
 
-	private boolean sendRequestAttachVm(List<VMDescriptor> vmList) {
+  private boolean sendRequestAttachVm(List<VMDescriptor> vmList) {
 
-		int n = 0;
-		while (true) {
-			String msg = "input [0-" + (vmList.size() - 1) + "] to choose vm! ";
-			String l;
-			l = readConsoleLine();
-			if (l != null) {
-				try {
-					n = Integer.parseInt(l.trim());
-				} catch (NumberFormatException e) {
-					handleText(msg);
-					continue;
-				}
+    int n = 0;
+    while (true) {
+      String msg = "input [0-" + (vmList.size() - 1) + "] to choose vm! ";
+      String l;
+      l = readConsoleLine();
+      if (l != null) {
+        try {
+          n = Integer.parseInt(l.trim());
+        } catch (NumberFormatException e) {
+          handleText(msg);
+          continue;
+        }
 
-			}
-			if (n < 0 || n >= vmList.size()) {
-				handleText(msg);
-				continue;
-			}
-			break;
-		}
-		Command c = new Command();
-		c.setName(Constants.REQUEST_ATTACH_VM);
-		c.setArgs(vmList.get(n).getPid());
-		Result result = sendSyncCommand(c);
-		return result.isSuccess();
-	}
+      }
+      if (0 <= n && n < vmList.size()) {
+        break;
+      } else {
+        boolean found = false;
+        for (int i = 0; i < vmList.size(); i++) {
+          VMDescriptor vm = vmList.get(i);
+          if (StringHelper.equals(vm.getPid(), String.valueOf(n))) {
+            n = i;
+            found = true;
+            break;
+          }
+        }
+        if (found) {
+          break;
+        }
+      }
+      handleText(msg);
+      continue;
 
-	public void start() {
+    }
+    Command c = new Command();
+    c.setName(Constants.REQUEST_ATTACH_VM);
+    c.setArgs(vmList.get(n).getPid());
+    Result result = sendSyncCommand(c);
+    return result.isSuccess();
+  }
 
-		Thread th = new Thread(new ConsoleWorker(), "knife-console-worker");
-		th.setDaemon(true);
-		th.start();
+  public void start() {
 
-	}
+    Thread th = new Thread(new ConsoleWorker(), "knife-console-worker");
+    th.setDaemon(true);
+    th.start();
 
-	public final void handleText(String line) {
-		writeLogLine(line);
-		writeConsoleLine(line);
-	}
+  }
 
-	private void writeLogLine(String line) {
-		try {
-			fileLog.write(line + "\n");
-			fileLog.flush();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+  public final void handleText(String line) {
+    writeLogLine(line);
+    writeConsoleLine(line);
+  }
 
-	public abstract String readConsoleLine();
+  private void writeLogLine(String line) {
+    try {
+      fileLog.write(line + "\n");
+      fileLog.flush();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
 
-	public abstract void writeConsoleLine(String line);
+  public abstract String readConsoleLine();
 
-	public abstract void setCompletors(String[]... strs);
+  public abstract void writeConsoleLine(String line);
 
-	private class ClientCompletorProcesser implements Completable {
-		private String[] cmdCompletors;
+  public abstract void setCompletors(String[]... strs);
 
-		@Override
-		public void setArgCompletors(String[] strs) {
-			setCompletors(cmdCompletors, strs);
-		}
+  private class ClientCompletorProcesser implements Completable {
+    private String[] cmdCompletors;
 
-		@Override
-		public void setCmdCompletors(String[] strs) {
-			cmdCompletors = strs;
-		}
+    @Override
+    public void setArgCompletors(String[] strs) {
+      setCompletors(cmdCompletors, strs);
+    }
 
-	}
+    @Override
+    public void setCmdCompletors(String[] strs) {
+      cmdCompletors = strs;
+    }
 
-	private class ClientPrinter extends Printer {
+  }
 
-		@Override
-		public void info(String str) {
-			if (str == null) {
-				return;
-			}
-			try {
-				writeConsoleLine(str);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
+  private class ClientPrinter extends Printer {
 
-		@Override
-		public void debug(String str) {
-			if (str == null) {
-				return;
-			}
-			try {
-				writeConsoleLine(str);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
+    @Override
+    public void info(String str) {
+      if (str == null) {
+        return;
+      }
+      try {
+        writeConsoleLine(str);
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
 
-	};
+    @Override
+    public void debug(String str) {
+      if (str == null) {
+        return;
+      }
+      try {
+        writeConsoleLine(str);
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
 
-	private class CommandResponseCallback extends ResponseCallbackTemplate {
-		@SuppressWarnings({ "rawtypes", "unchecked" })
-		private void onSuccess(Object content) {
-			if (content != null) {
-				if (content != null) {
-					TypePrintFormater formater = formaterManager.get(content
-							.getClass());
-					formater.printObject(content);
-				}
-			}
-		}
+  };
 
-		private void onError(String errorMessage, String errorTrace) {
-			writeConsoleLine(errorMessage);
-			if (errorTrace != null) {
-				for (String line : StringHelper.split(errorTrace, '\n')) {
-					writeConsoleLine(line);
-				}
-			}
-		}
+  private class CommandResponseCallback extends ResponseCallbackTemplate {
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    private void onSuccess(Object content) {
+      if (content != null) {
+        if (content != null) {
+          TypePrintFormater formater = formaterManager.get(content.getClass());
+          formater.printObject(content);
+        }
+      }
+    }
 
-		@Override
-		public void onPart(ResultPart part) {
-			try {
-				onSuccess(part.getContent());
+    private void onError(String errorMessage, String errorTrace) {
+      writeConsoleLine(errorMessage);
+      if (errorTrace != null) {
+        for (String line : StringHelper.split(errorTrace, '\n')) {
+          writeConsoleLine(line);
+        }
+      }
+    }
 
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
+    @Override
+    public void onPart(ResultPart part) {
+      try {
+        onSuccess(part.getContent());
 
-		@Override
-		public void done(ResultModel r) {
-			try {
-				if (r.isSuccess()) {
-					onSuccess(r.getContent());
-				} else {
-					onError(r.getErrorMessage(), r.getErrorTrace());
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
 
-		}
+    @Override
+    public void done(ResultModel r) {
+      try {
+        if (r.isSuccess()) {
+          onSuccess(r.getContent());
+        } else {
+          onError(r.getErrorMessage(), r.getErrorTrace());
+        }
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
 
-	};
+    }
 
-	private class ConsoleWorker implements Runnable {
+  };
 
-		public void run() {
-			List<VMDescriptor> vms = sendListVm();
-			if (vms == null) {
-				return;
-			}
-			boolean connected = sendRequestAttachVm(vms);
-			if (connected) {
-				// 获取命令列表
-				Command c = new Command();
-				c.setName("cmd");
-				onCommand(c);
-				while (true) {
-					String l = null;
-					try {
-						l = readConsoleLine();
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-					if (l != null) {
-						Command command = new Command();
-						command.setName(StringHelper.substringBefore(l, " "));
-						command.setArgs(StringHelper.substringAfter(l, " "));
-						onCommand(command);
-					}
-				}
-			}
-		}
-	}
+  private class ConsoleWorker implements Runnable {
+
+    public void run() {
+      List<VMDescriptor> vms = sendListVm();
+      if (vms == null) {
+        return;
+      }
+      boolean connected = sendRequestAttachVm(vms);
+      if (connected) {
+        // 获取命令列表
+        Command c = new Command();
+        c.setName("cmd");
+        onCommand(c);
+        while (true) {
+          String l = null;
+          try {
+            l = readConsoleLine();
+          } catch (Exception e) {
+            e.printStackTrace();
+          }
+          if (l != null) {
+            Command command = new Command();
+            command.setName(StringHelper.substringBefore(l, " "));
+            command.setArgs(StringHelper.substringAfter(l, " "));
+            onCommand(command);
+          }
+        }
+      }
+    }
+  }
 }
